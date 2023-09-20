b97351c373ecbfb914fc2a0d5b99e241a403b2c6
Elina Kerola <elinak90@icloud.com>
2023-09-20 15:01:18 +0200

    la till knapp och imageviev komponent på userscreen, ska göra en egen page

--- Components/ImageViewer.tsx
+++ Components/ImageViewer.tsx
@@ -1,22 +1,26 @@
-// import { StyleSheet, Image } from "react-native";
+import React from "react";
+import { StyleSheet, Image, ImageSourcePropType } from "react-native";
 
-// export default function ImageViewer({ placeholderImageSource, selectedImage }) {
-//   const imageSource = selectedImage
-//     ? { uri: selectedImage }
-//     : placeholderImageSource;
-//   return (
-//     <Image
-//       source={imageSource}
-     
-//       style={styles.image}
-//     />
-//   );
-// }
+interface ImageViewerProps {
+  placeholderImageSource: ImageSourcePropType;
+  selectedImage?: string | null;
+}
 
-// const styles = StyleSheet.create({
-//   image: {
-//     width: 320,
-//     height: 440,
-//     borderRadius: 18,
-//   },
-// });
+export default function ImageViewer({
+  placeholderImageSource,
+  selectedImage,
+}: ImageViewerProps) {
+  const imageSource = selectedImage
+    ? { uri: selectedImage }
+    : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
--- Pages/UserScreen.tsx
+++ Pages/UserScreen.tsx
@@ -1,41 +1,69 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { useNavigation, NavigationProp } from "@react-navigation/native";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, StatusBar  } from "react-native";
+import { useNavigation, NavigationProp, useNavigationState } from "@react-navigation/native";
+import ImageViewer from "../Components/ImageViewer";
+import * as ImagePicker from 'expo-image-picker';
+import ButtonImageViev from '../Components/ButtonImageViev';
 
-type FirstScreenProps = {
+type UserScreenProps = {
   navigation: NavigationProp<any>;
 };
 
-const UserScreen: React.FC<FirstScreenProps> = () => {
+const UserScreen: React.FC<UserScreenProps> = () => {
   const navigation = useNavigation<NavigationProp<any>>();
+  const PlaceholderImage = require('../assets/background-imagee.png');
 
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Välkommen</Text>
+      <View style={styles.imageContainer}>
+      <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      <View style={styles.footerContainer}>
+      <ButtonImageViev theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <ButtonImageViev label="Use this photo" />
+      </View>
+      
+
+
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: '#ffff',
+    alignItems: 'center',
   },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
-  button: {
-    backgroundColor: "pink", // Bakgrundsfärg för knappen
-    padding: 15,
-    borderRadius: 5,
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
-  buttonText: {
-    color: "white",
-    fontSize: 20,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
 
